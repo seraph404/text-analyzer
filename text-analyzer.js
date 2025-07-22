ec7ff20d7cbb1820a972a5999b8d32e39c803bb4
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-07-22 15:41:17 -0400

    Add code to pass mostCommonWord test

--- text-analyzer.js
+++ text-analyzer.js
@@ -2,8 +2,30 @@ function wordCount(text) {
   return text.trim().split(/\s+/).length;
 }
 
-wordCount("Hello world");
+function mostCommonWord(text) {
+  const wordArr = text.trim().split(/\s+/);
+  const wordCounts = wordArr.reduce((acc, word) => {
+    // if acc[word] doesn't exist, give it a beginning count of zero
+    // then, add one
+    acc[word] = (acc[word] || 0) + 1;
+    return acc;
+  }, {});
+  let mostCommon = null;
+  let highestCount = 0;
+
+  for (const word in wordCounts) {
+    if (wordCounts[word] > highestCount) {
+      highestCount = wordCounts[word];
+      mostCommon = word;
+    }
+  }
+
+  return mostCommon;
+}
+
+console.log(mostCommonWord("Hello world world"));
 
 module.exports = {
   wordCount,
+  mostCommonWord,
 };
