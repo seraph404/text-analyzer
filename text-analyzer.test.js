40a80669d090a55b375182f50d536a4ab1eadd14
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-07-22 14:46:01 -0400

    Add additional (passing) tests for trailing whitespace and new lines and tabs

--- text-analyzer.test.js
+++ text-analyzer.test.js
@@ -9,7 +9,16 @@ describe("textAnalyzer - wordCount", () => {
   test("accounts for leading whitespace in ' Hello, world!", () => {
     expect(wordCount(" Hello, world!")).toBe(2);
   });
+  test("accounts for trailing whitespace in 'Hello, world! ", () => {
+    expect(wordCount("Hello, world! ")).toBe(2);
+  });
   test("accounts for tabs", () => {
     expect(wordCount("Hello\tworld")).toBe(2);
+  });
+  test("accounts for new lines", () => {
+    expect(wordCount("Hello\nworld")).toBe(2);
+  });
+  test("accounts for new lines and tabs", () => {
+    expect(wordCount("Hello \n \t world")).toBe(2);
   });
 });
